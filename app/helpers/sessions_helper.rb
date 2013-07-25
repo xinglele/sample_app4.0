6789c629cd7f52d6ba8e7234a5fa1254c14572fd
liuxiangyu <liuxiangyu@liuxiangyu.(none)>
2013-07-25 17:25:37 +0800

    Add user micropost

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,4 +38,11 @@ module SessionsHelper
     session[:return_to] = request.fullpath
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
 end
